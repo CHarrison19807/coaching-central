74c79230d7ed54741c1cadd01b5a359f1f9b5807
Connor Harrison <connorh.uottawa@gmail.com>
2024-02-03 23:41:34 -0500

    style: create sign-up page

---
+++ src/app/(pages)/(auth)/sign-up/page.tsx
@@ -0,0 +1,145 @@
+"use client";
+
+import { Button, buttonVariants } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { cn } from "@/lib/utils";
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { trpc } from "@/trpc/client";
+import { toast } from "sonner";
+import { ZodError } from "zod";
+import { useRouter } from "next/navigation";
+import { GiTank } from "react-icons/gi";
+import { z } from "zod";
+import {
+  TSignUpCredentials,
+  signUpCredentials,
+} from "@/lib/validators/authValidator";
+
+function SignUpPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(signUpCredentials),
+  });
+
+  const router = useRouter();
+
+  const { mutate, isLoading } = trpc.auth.createPayloadUser.useMutation({
+    onError: (error) => {
+      if (error.data?.code === "CONFLICT") {
+        toast.error("This email is already in use");
+        return;
+      }
+      if (error instanceof ZodError) {
+        toast.error(error.issues[0].message);
+        return;
+      }
+      toast.error("Something went wrong. Please try again.");
+    },
+
+    onSuccess: ({ sentToEmail }) => {
+      toast.success(`Verification email sent to ${sentToEmail}`);
+      // router.push(`/verify-email?to=${sentToEmail}`);
+    },
+  });
+
+  function onSubmit({ email, password, confirmPassword }: TSignUpCredentials) {
+    mutate({ email, password, confirmPassword });
+  }
+
+  return (
+    <>
+      <div className="container relative flex pt-20 flex-col items-center justify-center lg:px-0">
+        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+          <div className="flex flex-col space-y-2 items-center text-center">
+            <GiTank className="w-20 h-20" />
+            <h1 className="text-2xl font-bold">Create an account</h1>
+            <Link
+              href="sign-in"
+              className={buttonVariants({ variant: "link" })}
+            >
+              Already have an account? Sign-in
+              <ArrowRight className="w-4 h-4" />
+            </Link>
+          </div>
+          <div className="grid gap-6">
+            <form
+              onSubmit={
+                // @ts-expect-error don't know why this is happening
+                handleSubmit(onSubmit)
+              }
+            >
+              <div className="grid gap-2">
+                <div className="grid gap-1 py-2">
+                  <Label htmlFor="email" className="pb-1">
+                    Email
+                  </Label>
+                  <Input
+                    id="email"
+                    {...register("email")}
+                    className={cn({
+                      "focus-visible:ring-red-500": errors.email,
+                    })}
+                    placeholder="email@example.com"
+                  />
+                  {errors?.email && (
+                    <p className="text-sm text-red-500">
+                      {errors.email.message?.toString()}
+                    </p>
+                  )}
+                </div>
+                <div className="grid gap-1 py-2">
+                  <Label htmlFor="password" className="pb-1">
+                    Password
+                  </Label>
+                  <Input
+                    id="password"
+                    {...register("password")}
+                    type="password"
+                    className={cn({
+                      "focus-visible:ring-red-500": errors.password,
+                    })}
+                    placeholder="password"
+                  />
+                  {errors?.password && (
+                    <p className="text-sm text-red-500">
+                      {errors.password.message?.toString()}
+                    </p>
+                  )}
+                </div>
+                <div className="grid gap-1 py-2">
+                  <Label htmlFor="confirmPassword" className="pb-1">
+                    Confirm Password
+                  </Label>
+                  <Input
+                    id="confirmPassword"
+                    {...register("confirmPassword")}
+                    type="password"
+                    className={cn({
+                      "focus-visible:ring-red-500": errors.confirmPassword,
+                    })}
+                    placeholder="confirm password"
+                  />
+                  {errors?.confirmPassword && (
+                    <p className="text-sm text-red-500">
+                      {errors.confirmPassword.message?.toString()}
+                    </p>
+                  )}
+                </div>
+                <Button>Sign up</Button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default SignUpPage;
