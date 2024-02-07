bd54132abb59ae3c3c7408184ccdba49f35fa7f3
Connor Harrison <connorh.uottawa@gmail.com>
2024-02-07 18:05:46 -0500

    perf: various minor changes

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import { getPayloadClient } from "./get-payload";
 import { nextApp, nextHandler } from "./next-utils";
 import * as trpcExpress from "@trpc/server/adapters/express";
 import { appRouter } from "./trpc";
+import { inferAsyncReturnType } from "@trpc/server";
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
@@ -14,6 +15,8 @@ const createContext = ({
   res,
 });
 
+export type ExpressContext = inferAsyncReturnType<typeof createContext>;
+
 const startServer = async () => {
   const payload = await getPayloadClient({
     initOptions: {
@@ -23,10 +26,12 @@ const startServer = async () => {
       },
     },
   });
+
   app.use(
     "/api/trpc",
     trpcExpress.createExpressMiddleware({ router: appRouter, createContext })
   );
+
   app.use((req, res) => nextHandler(req, res));
 
   nextApp.prepare().then(() => {
