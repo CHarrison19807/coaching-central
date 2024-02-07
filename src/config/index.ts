b593c282f24adb2810d9840538a21ce41c2dcfac
Connor Harrison <connorh.uottawa@gmail.com>
2024-02-07 18:00:26 -0500

    refactor: move commonly used functions and variables

---
+++ src/config/index.ts
@@ -0,0 +1,35 @@
+export const TANK_CATEGORIES = [
+  "Medium Tank",
+  "Light Tank",
+  "Heavy Tank",
+  "Tank Destroyer",
+  "Artillery",
+];
+
+export const TANK_NATIONS = [
+  "USA",
+  "Germany",
+  "USSR",
+  "UK",
+  "France",
+  "Czech",
+  "Japan",
+  "China",
+  "Poland",
+  "Sweden",
+  "Italy",
+];
+
+export const TANK_NATIONS_REFERENCE = [
+  "American",
+  "German",
+  "Russian",
+  "British",
+  "French",
+  "Czech",
+  "Japanese",
+  "Chinese",
+  "Polish",
+  "Swedish",
+  "Italian",
+];
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,42 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { Coach, ReplayOfTheWeek, TankOfTheWeek } from "@/payload-types";
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatPrice(
+  price: number | string,
+  options: {
+    currency?: "USD" | "EUR" | "GBP" | "BDT";
+    notation?: Intl.NumberFormatOptions["notation"];
+  } = {}
+) {
+  const { currency = "USD", notation = "compact" } = options;
+
+  const numericPrice = typeof price === "string" ? parseFloat(price) : price;
+
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency,
+    notation,
+    maximumFractionDigits: 2,
+  }).format(numericPrice);
+}
+
+export function getValidUrls(
+  quantity: number,
+  item: ReplayOfTheWeek | TankOfTheWeek | Coach
+) {
+  // TODO: Add support for multiple images
+  if (quantity === 1) {
+    return (
+      item.image
+        ? typeof item.image === "number"
+          ? item.image
+          : item.image.url
+        : null
+    ) as string;
+  }
 }
