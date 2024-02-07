48541f454bd9970c8a61e7aa4a02b96b020fd623
Connor Harrison <connorh.uottawa@gmail.com>
2024-02-06 19:21:56 -0500

    chore: configure payload types

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -4,6 +4,13 @@ import { postgresAdapter } from "@payloadcms/db-postgres";
 import { slateEditor } from "@payloadcms/richtext-slate";
 import path from "path";
 import dotenv from "dotenv";
+import { Users } from "./collections/Users/index";
+import { Coaches } from "./collections/Coaches/index";
+import { Media } from "./collections/Media/index";
+import { TankOfTheWeek } from "./collections/TankOfTheWeek";
+import { ReplayOfTheWeek } from "./collections/ReplayOfTheWeek";
+import { Orders } from "./collections/Orders";
+import { Sessions } from "./collections/Sessions";
 
 dotenv.config({
   path: path.resolve(__dirname, "../.env"),
@@ -11,11 +18,20 @@ dotenv.config({
 
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || "",
-  collections: [],
+  collections: [
+    Users,
+    Coaches,
+    Media,
+    TankOfTheWeek,
+    ReplayOfTheWeek,
+    Orders,
+    Sessions,
+  ],
   routes: {
     admin: "/admin",
   },
   admin: {
+    user: "users",
     bundler: webpackBundler(),
     meta: {
       titleSuffix: "- CoachingCentral",
